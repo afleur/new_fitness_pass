b6196b0d011ba3dc1a115df5636fe52ea800f188
Alois Fleuricourt <afleuricourt@gmail.com>
2019-02-25 16:21:47 +0100

    Methods index and show - SessionsController

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,39 @@
+class BookingsController < ApplicationController
+  def index
+    @booking = Booking.find(params[:id])
+  end
+
+  def show
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @session = Session.find(params[:session_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.session = @session
+    if @booking.save
+      redirect_to session_booking_path(@session, @booking), notice: 'Votre session est réservée.'
+    else
+      redirect_to session_path(@session), alert: 'Pb'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+  end
+end
