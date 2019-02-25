59ddb2719d8ea646c1deb92e6445f34a5ed206c5
Alois Fleuricourt <afleuricourt@gmail.com>
2019-02-25 15:09:04 +0100

    SessionsController added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+  get 'sessions/show'
+  get 'sessions/create'
+  get 'sessions/new'
+  get 'sessions/destroy'
+  get 'sessions/update'
+  get 'sessions/edit'
   devise_for :users
   root to: 'pages#home'
+  resources :sessions, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
