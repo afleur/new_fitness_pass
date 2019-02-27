ad9061cf4ff98436cf69e7df12eebbcdb4d62777
Alois Fleuricourt <afleuricourt@gmail.com>
2019-02-27 14:23:28 +0100

    coordinates added

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
   has_many :sessions, dependent: :destroy
   mount_uploader :photo, PhotoUploader
+  geocoded_by :street, :NPA, :city
+  after_validation :geocode, if: :will_save_change_to_address?
 end
