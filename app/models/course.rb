0fef00596556907c11215051df07aace7289d53d
Alois Fleuricourt <afleuricourt@gmail.com>
2019-02-27 14:38:48 +0100

    map on the show

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   has_many :sessions, dependent: :destroy
   mount_uploader :photo, PhotoUploader
-  geocoded_by :street, :NPA, :city
+  geocoded_by :city
   after_validation :geocode, if: :will_save_change_to_address?
 end
