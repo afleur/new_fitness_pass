04e8cba09f8916e95fae557bb1f94d0635ff34b7
Alois Fleuricourt <afleuricourt@gmail.com>
2019-02-27 16:33:49 +0100

    map added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_182743) do
+ActiveRecord::Schema.define(version: 2019_02_27_143657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_182743) do
     t.string "studio"
     t.string "photo"
     t.string "street"
-    t.integer "NPA"
+    t.integer "npa"
     t.float "latitude"
     t.float "longitude"
   end
