6a6ed45b260cc396164793ac04373a4a93f919f1
Alois Fleuricourt <afleuricourt@gmail.com>
2019-03-01 12:23:31 +0100

    index des bookings

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :bookings, only: [:show, :index, :new, :create]
   end
   get '/bookings/:booking_id/confirmation', to: 'bookings#confirmation', as: :confirmation
+  get '/users/:user_id/bookings', to: 'bookings#index', as: :mybookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
