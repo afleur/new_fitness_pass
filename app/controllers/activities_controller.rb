ae80e3df9e0ad9747c99d873d5b1672fc764fde2
Gieri Cosson <gieri@infomaniak.ch>
2019-03-05 10:29:49 +0100

    create js files

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,16 +6,17 @@ class ActivitiesController < ApplicationController
       @activities = Activity.all
     elsif params[:category].present?
       @activities = Activity.joins(:course).where("courses.category = ?", params[:category])
+      # @activities = Activity.joins(:course).where("courses.category = ?", params[:category])
     else
       @activities = Activity.global_search(params[:query])
     end
     today = Date.today-5
-    @activitiesdayone = @activities.where(start_time: today.beginning_of_day..today.end_of_day)
-    @activitiesdaytwo = @activities.where(start_time: (today + 1).beginning_of_day..(today + 1).end_of_day)
+    @activitiesdayone   = @activities.where(start_time: today.beginning_of_day..today.end_of_day)
+    @activitiesdaytwo   = @activities.where(start_time: (today + 1).beginning_of_day..(today + 1).end_of_day)
     @activitiesdaythree = @activities.where(start_time: (today + 2).beginning_of_day..(today + 2).end_of_day)
-    @activitiesdayfour = @activities.where(start_time: (today + 3).beginning_of_day..(today + 3).end_of_day)
-    @activitiesdayfive = @activities.where(start_time: (today + 4).beginning_of_day..(today + 4).end_of_day)
-    @activitiesdaysix = @activities.where(start_time: (today + 5).beginning_of_day..(today + 5).end_of_day)
+    @activitiesdayfour  = @activities.where(start_time: (today + 3).beginning_of_day..(today + 3).end_of_day)
+    @activitiesdayfive  = @activities.where(start_time: (today + 4).beginning_of_day..(today + 4).end_of_day)
+    @activitiesdaysix   = @activities.where(start_time: (today + 5).beginning_of_day..(today + 5).end_of_day)
     @activitiesdayseven = @activities.where(start_time: (today + 6).beginning_of_day..(today + 6).end_of_day)
      @markers = @activities.map do |activity|
       {
