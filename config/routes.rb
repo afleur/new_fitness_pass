4cf1be7b1818116d5cac112c1fd809a4129c1b6d
Alois Fleuricourt <afleuricourt@gmail.com>
2019-03-05 08:03:05 +0100

    UsersController created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   get 'orders/index'
   get 'orders/show'
   get 'orders/new'
@@ -16,5 +20,6 @@ Rails.application.routes.draw do
   get '/bookings/:booking_id/confirmation', to: 'bookings#confirmation', as: :confirmation
   get '/users/:user_id/bookings', to: 'bookings#index', as: :mybookings
   get '/users/:user_id/bookings/:booking_id', to: 'bookings#show', as: :bookingshow
+  get '/users/:user_id/credits_account', to: 'user#credits', as: :creditsuser
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
