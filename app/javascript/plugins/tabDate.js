803f9f48e2eafacc868e7b8475fad37ace85655c
Gieri Cosson <gieri@infomaniak.ch>
2019-03-06 10:58:46 +0100

    js fixed

--- app/javascript/plugins/tabDate.js
+++ app/javascript/plugins/tabDate.js
@@ -1,1 +1,18 @@
-tabDate.js
+const clickDateTab = () => {
+  const tab = document.querySelector('.indextab-wrap');
+  if (tab) {
+    const params = window.location.search;
+    const position = params.search("date");
+    const today = params[position + 13] + params[position + 14];
+    const tabs = document.getElementsByClassName("indextab-label-content");
+    tabs[0].getElementsByTagName('label')[0].click()
+    for (let item of tabs) {
+      if (item.innerText.search(today) > 0) {
+        item.getElementsByTagName('label')[0].click()
+      }
+    };
+  }
+};
+
+
+export { clickDateTab };
