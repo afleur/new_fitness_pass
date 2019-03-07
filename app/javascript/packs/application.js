58377e08f02da55827c104878cce1bbd15a2be1e
Alois Fleuricourt <afleuricourt@gmail.com>
2019-03-07 15:20:21 +0100

    map ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,41 +1,19 @@
 import "bootstrap";
-
 import 'mapbox-gl/dist/mapbox-gl.css';
-// <-- you need to uncomment the stylesheet_pack_tag in the layout!
-// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
-
 import { initMapbox } from '../plugins/init_mapbox';
-import { clickDateTab } from '../plugins/tabDate';
-
-import { initialMouse, slideMovementTotal, mouseIsDown, slider, currentMouse, relativeMouse, slidePercent } from './swipebooking';
-// import { function, setTimeout } from './circle';
-
-import { loadDynamicBannerText } from '../plugins/validation';
+initMapbox();
 
 import { removeNavbar } from '../plugins/confirmation';
-import { bar, value, ProgressBar, line  } from '../plugins/circle-progress';
-
-const slide = document.querySelector(".slide-text")
-
 removeNavbar();
-initMapbox();
+
+import { clickDateTab } from '../plugins/tabDate';
 clickDateTab();
+
+import { loadDynamicBannerText } from '../plugins/validation';
 loadDynamicBannerText();
-bar();
-value();
-ProgressBar();
-line();
+
+import {  } from '../plugins/circle-progress';
 
 
 
-if (slide) {
-  initialMouse();
-  slideMovementTotal();
-  mouseIsDown();
-  slider();
-  currentMouse();
-  relativeMouse();
-  slidePercent();
-}
-
-// setTimeout();
+import { } from './swipebooking';
