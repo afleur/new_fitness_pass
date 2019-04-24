948b301fda4bb5245b4cfef22293118c63968c4e
Alois Fleuricourt <afleuricourt@gmail.com>
2019-04-24 16:06:18 +0200

    creation of coach model db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_05_134527) do
+ActiveRecord::Schema.define(version: 2019_04_24_140459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 2019_03_05_134527) do
     t.datetime "updated_at", null: false
     t.index ["activity_id"], name: "index_bookings_on_activity_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "coaches", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "category"
+    t.datetime "birth_date"
+    t.string "photo_url"
+    t.string "email"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "courses", force: :cascade do |t|
