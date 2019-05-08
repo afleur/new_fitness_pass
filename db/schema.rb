5bb417809ef4d00e2794793c5002459099b2c5c8
Alois Fleuricourt <afleuricourt@gmail.com>
2019-05-08 14:23:36 +0200

    added capacity to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_125044) do
+ActiveRecord::Schema.define(version: 2019_05_08_122122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_125044) do
     t.bigint "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "capacity"
     t.index ["course_id"], name: "index_activities_on_course_id"
   end
 
