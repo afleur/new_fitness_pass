7c143a7fae0dc0d3cb6fd17c2a382834852ec515
Alois Fleuricourt <afleuricourt@gmail.com>
2019-05-08 07:25:41 +0200

    WIP coach model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,8 @@ class PagesController < ApplicationController
     @coach = current_coach
     courses = Course.all
     @coach.nil? ? "": @courses_of_current_coach =  courses.select { |course| course.coach_id == @coach.id }
+    @activities = Activity.all
+    @activities.select {|activity| activity.course_id == @courses_of_current_coach.id}
   end
 end
 
