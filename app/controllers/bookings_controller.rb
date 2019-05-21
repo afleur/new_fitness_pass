5992da2f80f551cd2388a8442cda4293a5464799
Alois Fleuricourt <afleuricourt@gmail.com>
2019-05-21 14:49:36 +0200

    WIP coach bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ require 'time'
 
 class BookingsController < ApplicationController
   skip_before_action :authenticate_user!
+  helper :all
 
   def index
       @pastbookings = []
@@ -19,44 +20,138 @@ class BookingsController < ApplicationController
     end
 
       def index_coach
-      @courses_of_current_coach = []
-      @activities_of_current_coach = []
-      @comingactivities = []
-      @pastactivities = []
-      @nb_of_bookings = []
-      @counter = []
+      # @courses_of_current_coach = []
+      # @activities_of_current_coach = []
+      # @nb_of_bookings = []
+      # @counter = []
+      # @courses = Course.all
       @coach = current_coach
-      @courses = Course.all
-      @bookings = Booking.all
+      # @bookings = Booking.all
       if @coach.nil?
         return nil
       else
-        @courses_of_current_coach << @courses.select { |course| course.coach_id == current_coach.id }
-        @activities = Activity.all
-        @activities.each do |activity|
-          @courses_of_current_coach.each do |course|
-            course.each do |x|
-              if x.id == activity.course_id
-                @activities_of_current_coach << activity
-              end
+        coach_courses
+        coach_activities
+        coming_activities
+        # registered_people(activity_id)
+        @registered_people = []
+        @bookings = Booking.all
+        @bookings.each do |booking|
+          @coming_activities.each do |x|
+            if booking.activity_id == x.id
+              @registered_people << booking
             end
           end
         end
-        @activities_of_current_coach.each do |activity|
-          if activity.start_time > Time.now
-            @comingactivities << activity
-          else
-            @pastactivities << activity
-          end
       end
-        @nb_of_bookings = @bookings.select do |booking|
-          @comingactivities.each do |x|
-            booking.activity_id == x.id
-          end
+      #   @coach = current_coach
+      #   @courses = Course.all
+      #   @courses_of_current_coach = []
+      #   @courses.each do |course|
+      #     if course.coach_id == current_coach.id
+      #     @courses_of_current_coach << course
+      #     end
+      #   end
+      #   @activities = Activity.all
+      #   @activities_of_current_coach = []
+      #   @activities.each do |activity|
+      #     @courses_of_current_coach.each do |course|
+      #       if course.id == activity.course_id
+      #         @activities_of_current_coach << activity
+      #       end
+      #     end
+      #   end
+      #   @coming_activities = []
+      #   @past_activities = []
+      #   @activities_of_current_coach.each do |activity|
+      #     if activity.start_time > Time.now
+      #       @coming_activities << activity
+      #     else
+      #       @past_activities << activity
+      #     end
+      #   end
+      #   @registered_people = []
+      #   @bookings = Booking.all
+      #   @bookings.each do |booking|
+      #     @coming_activities.each do |x|
+      #       if activity_id == x.id
+      #         registered_people << booking
+      #       end
+      #     end
+      #   end
+      # end
+
+        # @courses_of_current_coach << @courses.select { |course| course.coach_id == current_coach.id }
+        # @activities = Activity.all
+        # @activities.each do |activity|
+        #   @courses_of_current_coach.each do |course|
+        #     course.each do |x|
+        #       if x.id == activity.course_id
+        #         @activities_of_current_coach << activity
+        #       end
+        #     end
+        #   end
+        # end
+
+      # end
+        # @nb_of_bookings = @bookings.select do |booking|
+        #   @comingactivities.each do |x|
+        #     booking.activity_id == x.id
+      #     end
+      #   end
+      # end
+    end
+
+
+def coach_courses
+  @coach = current_coach
+  @courses = Course.all
+  @courses_of_current_coach = []
+  @courses.each do |course|
+    if course.coach_id == current_coach.id
+    @courses_of_current_coach << course
+    end
+  end
+end
+
+def coach_activities
+  coach_courses
+  @activities = Activity.all
+  @activities_of_current_coach = []
+  @activities.each do |activity|
+    @courses_of_current_coach.each do |course|
+      if course.id == activity.course_id
+       @activities_of_current_coach << activity
         end
       end
     end
+  end
 
+def coming_activities
+  coach_activities
+  @coming_activities = []
+  @past_activities = []
+  @activities_of_current_coach.each do |activity|
+    if activity.start_time > Time.now
+      @coming_activities << activity
+    else
+      @past_activities << activity
+    end
+  end
+end
+
+  # def registered_people(activity_id)
+  #   coming_activities
+  #   @registered_people = []
+  #   @bookings = Booking.all
+  #   @bookings.each do |booking|
+  #     @coming_activities.each do |x|
+  #       if activity_id == x.id
+  #         registered_people << booking
+  #       end
+  #     end
+  #   end
+  # end
 
   def show
     @booking = Booking.find(params[:id])
@@ -100,5 +195,6 @@ class BookingsController < ApplicationController
   def confirmation
     @booking = Booking.find(params[:booking_id])
   end
+
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_151055) do
+ActiveRecord::Schema.define(version: 2019_05_20_091539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2019_05_09_151055) do
     t.bigint "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "capacity"
+    t.integer "capacity", default: 5
     t.index ["course_id"], name: "index_activities_on_course_id"
   end
 
